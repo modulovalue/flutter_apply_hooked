d97bdaa2f6b5e0cd888b1cfe2d90b5078d10a2e5
Modestas Valauskas <valauskasmodestas@gmail.com>
2019-11-05 00:14:03 +0100

    updated from single_base to single_bloc_base

--- lib/src/build_bloc.dart
+++ lib/src/build_bloc.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_apply_hooked/flutter_apply_hooked.dart';
-import 'package:single_base/single_base.dart';
+import 'package:single_bloc_base/single_bloc_base.dart';
 
 T $$$<T extends BlocBase>(T Function() bloc, {List<Object> keys}) =>
     useDispose(bloc, (a) => a.dispose(), keys: keys);
